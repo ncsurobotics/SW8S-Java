49b90a1cbcaf0f864acd3eb9e676837c943bfa1c
Achyuta Kannan <ackannan@ncsu.edu>
2022-09-14 14:28:47 -0400

    added stroke 6

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/StrokeSixState.java
@@ -0,0 +1,40 @@
+package org.aquapackrobotics.sw8s.states;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
+
+public class StrokeSixState extends SimState {
+
+    private SimWindow sim;
+    public StrokeSixState(ScheduledThreadPoolExecutor pool, SimWindow sim) {
+        super(pool, sim);
+        this.sim = sim;
+    }
+
+    @Override
+    public void onEnter() {
+        
+    }
+
+    @Override
+    public boolean onPeriodic() {
+        sim.setRobotSpeed(0, 0, -0.2);
+        if (sim.getRobotAngle() < -270) {
+            sim.setRobotSpeed(0, 0, 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onExit() {
+        
+    }
+
+    @Override
+    public State nextState() {
+        return new StrokeSevenState(pool, sim);
+    }
+    
+}
