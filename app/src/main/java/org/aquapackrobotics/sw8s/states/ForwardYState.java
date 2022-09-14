3aa0d40826d2a56f72913f6e073bfee35e195697
vpatil5 <vpatil5@ncsu.edu>
2022-09-14 13:56:02 -0400

    Added distance parameter to ForwardYState

--- app/src/main/java/org/aquapackrobotics/sw8s/states/ForwardYState.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/ForwardYState.java
@@ -7,9 +7,11 @@ import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
 public class ForwardYState extends SimState {
 	
 	double initialY;
+	double targetDistance;
 	
-    public ForwardYState(ScheduledThreadPoolExecutor pool, SimWindow sim) {
+    public ForwardYState(ScheduledThreadPoolExecutor pool, SimWindow sim, double distance) {
         super(pool, sim);
+        targetDistance = distance;
     }
 
     // TODO: implement
