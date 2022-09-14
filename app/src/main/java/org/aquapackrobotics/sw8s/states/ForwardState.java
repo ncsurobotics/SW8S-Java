82836ec919b1a7ff0004c7b1707784a3930123c7
vpatil5 <vpatil5@ncsu.edu>
2022-09-14 13:36:19 -0400

    Added ForwardState

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/ForwardState.java
@@ -0,0 +1,30 @@
+package org.aquapackrobotics.sw8s.states;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
+
+public class ForwardState extends SimState {
+	
+    public ForwardState(ScheduledThreadPoolExecutor pool, SimWindow sim) {
+        super(pool, sim);
+    }
+
+    // TODO: implement
+    public void onEnter() {
+    }
+
+    // TODO: implement
+    public boolean onPeriodic() {
+        return false;
+    }
+
+    // TODO: implement
+    public void onExit() {
+    }
+
+    // TODO: implement
+    public State nextState() {
+        return null;
+    }
+}
