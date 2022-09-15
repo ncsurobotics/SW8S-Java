07e3f4f5b3b29c0476cb5d0fb0f15a46b84f47fa
Blokz101 <mquantwenty22@gmail.com>
2022-09-14 21:31:41 -0400

    Added GShapeConstants class.

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/constants/GShapeConstants.java
@@ -0,0 +1,13 @@
+package org.aquapackrobotics.sw8s.constants;
+
+// import org.aquapackrobotics.sw8s.constants.Coordinate;
+
+public class GShapeConstants {
+    
+    public static Coordinate gCoord1 = new Coordinate(350, 100);
+    public static Coordinate gCoord2 = new Coordinate(100, 100);
+    public static Coordinate gCoord3 = new Coordinate(100, 400);
+    public static Coordinate gCoord4 = new Coordinate(350, 400);
+    public static Coordinate gCoord5 = new Coordinate(350, 250);
+
+}
