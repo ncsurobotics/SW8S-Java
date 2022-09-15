13d69ab1f4cd6cb89bed679b284bfe9a18eca3ba
Blokz101 <mquantwenty22@gmail.com>
2022-09-14 21:47:10 -0400

    InitState now moves to the start of the G.

--- app/src/main/java/org/aquapackrobotics/sw8s/states/InitState.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/InitState.java
@@ -3,26 +3,26 @@ package org.aquapackrobotics.sw8s.states;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
+import org.aquapackrobotics.sw8s.constants.GShapeConstants;
 
 public class InitState extends SimState {
     public InitState(ScheduledThreadPoolExecutor pool, SimWindow sim) {
         super(pool, sim);
     }
 
-    // TODO: implement
     public void onEnter() {
+        this.window.setRobotSpeed(0, -1, 0);
     }
 
-    // TODO: implement
     public boolean onPeriodic() {
-        return false;
+        System.out.print(""); //I do not understand why this is required to make this.window get the position correctly but it exists here now.
+        return this.window.getYPos() > GShapeConstants.gCoord1.getYCoord();
     }
 
-    // TODO: implement
     public void onExit() {
+        this.window.setRobotSpeed(0, 0, 0);
     }
 
-    // TODO: implement
     public State nextState() {
         return null;
     }
