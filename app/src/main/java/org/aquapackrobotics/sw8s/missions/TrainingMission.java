fceb1638276ba475e4f5e394911677c1290af74a
vpatil5 <vpatil5@ncsu.edu>
2022-09-14 23:19:25 -0400

    Added comments to TrainingMission

--- app/src/main/java/org/aquapackrobotics/sw8s/missions/TrainingMission.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/missions/TrainingMission.java
@@ -16,19 +16,30 @@ public class TrainingMission extends Mission {
         super(pool);
         sim = new SimWindow();
     }
-
+    
+    /*
+     * Returns an instance of ForwardYState as the first state
+     * Passes a distance of -400 to move the robot up 400 pixels 
+     */
     @Override
     protected State initialState() {
         return new ForwardYState(pool, sim, -400);
     }
-
+    
+    /*
+     * Executes current state
+     * Runs onEnter() first, then onPeriodic() while it returns true, then onExit()
+     */
     @Override
     protected void executeState(State state) {
     	state.onEnter();
     	while(state.onPeriodic()) {}
     	state.onExit();
     }
-
+    
+    /**
+     * Returns the next State from the current State's nextState() method
+     */
     @Override
     protected State nextState(State state) {
         return state.nextState();
