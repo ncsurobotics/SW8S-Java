17bc1f14d5c95a274a2c4113fa8a74dc78eeae90
vpatil5 <vpatil5@ncsu.edu>
2022-09-15 20:34:48 -0400

    Added fields to RotateLeftState

--- app/src/main/java/org/aquapackrobotics/sw8s/states/RotateLeftState.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/RotateLeftState.java
@@ -2,13 +2,19 @@ package org.aquapackrobotics.sw8s.states;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-public class RotateLeftState extends State {
-    public RotateLeftState(ScheduledThreadPoolExecutor pool) {
-        super(pool);
+import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
+
+public class RotateLeftState extends SimState {
+	
+	private final double kError = 1.0;
+	
+    public RotateLeftState(ScheduledThreadPoolExecutor pool, SimWindow sim) {
+        super(pool, sim);
     }
 
     // TODO: implement
     public void onEnter() {
+    	
     }
 
     // TODO: implement
