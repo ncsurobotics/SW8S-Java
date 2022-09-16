c5dac4269ea4575a0be372eb88a651b2534397ef
vpatil5 <vpatil5@ncsu.edu>
2022-09-15 21:17:21 -0400

    Added comments to ForwardXState

--- app/src/main/java/org/aquapackrobotics/sw8s/states/ForwardXState.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/ForwardXState.java
@@ -4,11 +4,17 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
 
+/**
+ * Moves robot a certain distance in the X-axis
+ */
 public class ForwardXState extends SimState {
 	
-	double initialX;
-	double targetDistance;
-	final double kError = 0.5;
+	//Initial X position of the robot
+	private double initialX;
+	//Target distance to be traveled by the robot
+	private double targetDistance;
+	//Minimum distance from target position to be considered done
+	private final double kError = 0.5;
 	
     public ForwardXState(ScheduledThreadPoolExecutor pool, SimWindow sim, double distance) {
         super(pool, sim);
@@ -18,9 +24,12 @@ public class ForwardXState extends SimState {
     public void onEnter() {
     	window.setRobotSpeed(0.0, 0.0, 0.0);
     	initialX = window.getXPos();
-    	System.out.println(targetDistance);
     }
-
+    
+    /*
+     * Moves robot at a speed of 1.0 to the target position.
+     * Once robot is within kError of the target position, returns false to signal completion
+     */
     public boolean onPeriodic() {
     	window.setRobotSpeed(Math.signum(targetDistance) * 1.0, 0.0, 0.0);
     	if (Math.abs(window.getXPos() - (initialX + targetDistance)) < kError)
@@ -33,6 +42,9 @@ public class ForwardXState extends SimState {
     	window.setRobotSpeed(0.0, 0.0, 0.0);
     }
 
+    /*
+     * Returns null to signal completion of mission
+     */
     public State nextState() {
     	return null;
     }
