212d619931456d9b8b5107d33d6a1d45e720bb3e
vpatil5 <vpatil5@ncsu.edu>
2022-09-15 21:33:07 -0400

    Cleaned up RotateLeftState
    Added multiplier and added comments

--- app/src/main/java/org/aquapackrobotics/sw8s/states/RotateLeftState.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/RotateLeftState.java
@@ -4,42 +4,52 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
 
+/**
+ * Rotates robot left by 90 degrees
+ */
 public class RotateLeftState extends SimState {
 	
-	private final double kError = 1.0;
+	//Target angle to be rotated to by the robot
 	private double targetAngle;
+	//Multiplier for setting yawVelocity
+	private final double kP = -2.0;
+	//Minimum degrees from target angle to be considered done
+	private final double kError = 0.5;
 	
     public RotateLeftState(ScheduledThreadPoolExecutor pool, SimWindow sim) {
         super(pool, sim);
     }
 
-    // TODO: implement
     public void onEnter() {
     	window.setRobotSpeed(0.0, 0.0, 0.0);
     	targetAngle = window.getRobotAngle() - 90;
+    	//Rounds targetAngle to nearest multiple of 90
+    	targetAngle = Math.round(targetAngle/(int)90) * 90;
     }
 
-    // TODO: implement
+    /**
+     * Turns robot at a speed of 1.0 to the target angle.
+     * Once robot is within kError of the target angle, returns false to signal completion
+     */
     public boolean onPeriodic() {
-    	double yawSpeed = -2.0 * (window.getRobotAngle() - targetAngle);
-    	window.setRobotSpeed(0.0, 0.0, yawSpeed);
+    	double yawVelocity = kP * (window.getRobotAngle() - targetAngle);
+    	window.setRobotSpeed(0.0, 0.0, yawVelocity);
     	
     	if (Math.abs(window.getRobotAngle() - targetAngle) < kError)
     		return false;
     	else
     		return true;
-    		
     }
 
-    // TODO: implement
     public void onExit() {
     	window.setRobotSpeed(0.0, 0.0, 0.0);
     }
 
-    // TODO: implement
+    /**
+     *  If robot is facing left, returns an instance of RotateState.
+     *  Else, returns an instance of ForwardXState
+     */
     public State nextState() {
-    	//If robot is facing left, returns an instance of RotateState
-    	//Else, returns an instance of ForwardXState
     	if (targetAngle == -90)
     		return new RotateState(pool, window, 100);
     	else
