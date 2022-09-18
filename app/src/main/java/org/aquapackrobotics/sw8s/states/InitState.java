c76e41752d41c72062c63d7e1a78e7136564a5e6
Blokz101 <mquantwenty22@gmail.com>
2022-09-18 16:48:39 -0400

    Initial move now uses MoveState.java and the first index of GShapeConstants.java

--- app/src/main/java/org/aquapackrobotics/sw8s/states/InitState.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/InitState.java
@@ -3,7 +3,6 @@ package org.aquapackrobotics.sw8s.states;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
-import org.aquapackrobotics.sw8s.Controller;
 import org.aquapackrobotics.sw8s.constants.GShapeConstants;
 
 public class InitState extends SimState {
@@ -11,25 +10,14 @@ public class InitState extends SimState {
         super(pool, sim);
     }
 
-    public void onEnter() {
-        this.window.setRobotSpeed(0, -1, 0);
-    }
+    public void onEnter() { }
 
-    public boolean onPeriodic() {
-        this.window.setRobotSpeed(
-            0, 
-            Controller.calculateVel(GShapeConstants.gCoord1.getYCoord(), this.window.getYPos()), 
-            0
-        );
+    public boolean onPeriodic() { return false; }
 
-        return this.window.getYPos() != GShapeConstants.gCoord1.getYCoord();
-    }
+    public void onExit() { }
 
-    public void onExit() {
-        this.window.setRobotSpeed(0, 0, 0);
-    }
-
-    public State nextState() {
-        return null;
+    public State nextState()
+     {
+        return new MoveState(pool, this.window, GShapeConstants.coords[0]);
     }
 }
