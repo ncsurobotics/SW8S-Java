05b8889019950f80e71e9f0143477b3d67ea8b8c
Blokz101 <mquantwenty22@gmail.com>
2022-09-19 00:37:19 -0400

    Updated Coordinate.java to include a getAngle method.

--- app/src/main/java/org/aquapackrobotics/sw8s/constants/Coordinate.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/constants/Coordinate.java
@@ -31,4 +31,27 @@ public class Coordinate
         return xCoordsMatch && yCoordsMatch;
     }
 
+
+    /**
+     * Converts from partesian coords to polar and returns theta in degrees.
+     * @param newCoord The second coord needed for the calculation.
+     * @return The angle in degrees from the first coord to the second.
+     */
+    public double getAngle(Coordinate newCoord)
+    {
+        double xDiff = newCoord.getXCoord() - getXCoord();
+        double yDiff = newCoord.getYCoord() - getYCoord();
+
+        double theta;
+
+        if(xDiff > 0) { theta = Math.atan(yDiff/xDiff); }
+        else if(xDiff < 0 && yDiff >= 0) { theta = Math.PI + Math.atan(yDiff/xDiff); }
+        else if(xDiff < 0 && yDiff < 0) { theta = -Math.PI + Math.atan(yDiff/xDiff); }
+        else if(xDiff == 0 && yDiff > 0) { theta = Math.PI/2.0; }
+        else if(xDiff == 0 && yDiff < 0) { theta = -Math.PI/2.0; }
+        else { theta = 0; }
+
+        return (theta * 180 / Math.PI) + 90;
+    }
+
 }
