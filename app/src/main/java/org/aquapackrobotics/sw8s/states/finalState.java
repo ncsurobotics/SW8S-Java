8fab0029dfc3d449a4b8ac54f778ef59ccba4f53
vsangarya <vishwesharya@gmail.com>
2022-09-19 12:06:21 -0400

    Add variables and getters in finalState

--- app/src/main/java/org/aquapackrobotics/sw8s/states/finalState.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/finalState.java
@@ -1,5 +1,24 @@
 package org.aquapackrobotics.sw8s.states;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-public class finalState {
-  
+public class finalState extends State{
+  public finalState(ScheduledThreadPoolExecutor pool) {
+    super(pool);
+  }
+
+  private double xVel = 0;
+  private double yVel = 0;
+  private double yawVel = 0;
+
+  public double getX(){
+    return xVel;
+  }
+
+  public double getY(){
+    return yVel;
+  }
+
+  public double getYaw(){
+    return yawVel;
+  }
 }
