96d811b2563da8b46647ae8fa36107e5d784f6ec
vsangarya <vishwesharya@gmail.com>
2022-09-19 12:03:48 -0400

    Update stateTwo

--- app/src/main/java/org/aquapackrobotics/sw8s/states/stateTwo.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/stateTwo.java
@@ -1,5 +1,27 @@
 package org.aquapackrobotics.sw8s.states;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-public class stateTwo {
+public class stateTwo extends State{
+  public stateTwo(ScheduledThreadPoolExecutor pool) {
+    super(pool);
+  }
+
+  //x and y velocities for Initial State
+  private double xVel = 0;
+  private double yVel = 0;
+  private double yawVel = 0;
+  int count = 0;
   
+
+  public double getX(){
+    return xVel;
+  }
+
+  public double getY(){
+    return yVel;
+  }
+
+  public double getYaw(){
+    return yawVel;
+  }
 }
