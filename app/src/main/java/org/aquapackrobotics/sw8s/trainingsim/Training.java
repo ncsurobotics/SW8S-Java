010fd2d6eeb0c2db2b5c4d025cc5fabcc26eeb27
jrainone14 <flyerjason25@gmail.com>
2022-09-20 20:36:43 -0400

    created a bunch of stuff

--- app/src/main/java/org/aquapackrobotics/sw8s/trainingsim/Training.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/trainingsim/Training.java
@@ -7,18 +7,18 @@ public class Training {
     public static void main(String args[]) throws Exception{
         SimWindow sim = new SimWindow();
 
-        double kp = 10;
-        double targetAngle = 45;
+        double kp = -100;
+        double targetAngle = -45;
 
-        Thread.sleep(1000);
+        Thread.sleep(100);
 
         long startTime = System.currentTimeMillis();
 
         while(true){
             double yawSpeed = kp * (sim.getRobotAngle() - targetAngle);
-            double ySpeed = (System.currentTimeMillis() - startTime < 1750) ? -0.85 : 0.0;
-            sim.setRobotSpeed(0, ySpeed, -yawSpeed);
-            Thread.sleep(10);
+            double ySpeed = (System.currentTimeMillis() - startTime < 1500) ? -1 : -.01;
+            sim.setRobotSpeed(10, ySpeed, yawSpeed);
+            Thread.sleep(100);
         }
     }
 }
