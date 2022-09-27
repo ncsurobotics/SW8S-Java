1862a96d600d4753de2f53b5fae61b7f68bc98a6
vpatil5 <vpatil5@ncsu.edu>
2022-09-27 19:58:42 -0400

    Added constructor to ControlBoardCommunication

--- app/src/main/java/org/aquapackrobotics/sw8s/comms/ControlBoardCommunication.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/ControlBoardCommunication.java
@@ -1,8 +1,20 @@
 package org.aquapackrobotics.sw8s.comms;
 
-public class ControlBoardCommunication {
+import com.fazecast.jSerialComm.*;
 
+public class ControlBoardCommunication {
+	private SerialPort controlBoardPort;
+	
+	private static int START_BYTE;
+	private static int END_BYTE;
     // Look in "Communication Protocol" under SW8E Control Board to find the spec
+	
+    public ControlBoardCommunication() {
+    	controlBoardPort = SerialPort.getCommPorts()[0];
+    }
     
+    private void writeBytesToBoard(byte[] byteArray) {
+    	 
+    }
 
 }
