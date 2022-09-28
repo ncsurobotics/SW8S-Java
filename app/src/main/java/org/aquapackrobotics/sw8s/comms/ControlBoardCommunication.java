675ae9655f4a3a559eb9e6937b3257b592039be1
Blokz101 <mquantwenty22@gmail.com>
2022-09-27 20:25:36 -0400

    Added skeleton of API

--- app/src/main/java/org/aquapackrobotics/sw8s/comms/ControlBoardCommunication.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/ControlBoardCommunication.java
@@ -1,8 +1,68 @@
 package org.aquapackrobotics.sw8s.comms;
 
+import com.fazecast.jSerialComm.*;
+
 public class ControlBoardCommunication {
 
-    // Look in "Communication Protocol" under SW8E Control Board to find the spec
-    
+    public enum ControlBoardMode { RAW, LOCAL };
+
+    //Constructor
+    public ControlBoardCommunication() {
+
+        // TODO: Impliment
+
+    }
+
+    /**
+     * Sets the mode of the control board.
+     * @param mode ControlBoardMode enum that is either RAW or LOCAL.
+     */
+    public void setMode(ControlBoardMode mode) {
+
+        // TODO: Impliment
+
+    }
+
+    /**
+     * Returns the mode the control board is in.
+     * @return ControlBoardMode enum that is either RAW or LOCAL.
+     */
+    public ControlBoardMode getMode() {
+
+        // TODO: Impliment
+
+        return null;
+    }
+
+    /**
+     * Sets the thruster inversions individually.
+     * @param invertThrusters Array of 8 booleans, each representing an individual thruster.
+     */
+    public void setThrusterInversions(boolean invert1, boolean invert2, boolean invert3, boolean invert4, boolean invert5, boolean invert6, boolean invert7, boolean invert8) {
+
+        // TODO: Impliment
+
+    }
+
+    /**
+     * Gets the current thruster inversions.
+     * @return Array of 8 booleans, each representing an individual thruster.
+     */
+    public boolean[] getThrusterInversions() {
+
+        // TODO: Impliment
+
+        return null;
+    }
+
+    /**
+     * Directly sets the speeds of the thrusters.
+     * @param speeds Array of 8 doubles that should range from -1 to 1, each representing an individual thruster.
+     */
+    public void setRawSpeeds(double speed1, double speed2, double speed3, double speed4, double speed5, double speed6, double speed7, double speed8) {
+
+        // TODO: Immpliment
+
+    }
 
 }
