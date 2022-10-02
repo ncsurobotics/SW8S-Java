4d371ed563ca805924ef74c1d80d7af34d477c31
ltvantil <lvantil@ncsu.edu>
2022-10-02 18:26:00 -0400

    Finished first pass of constructMessage

--- app/src/main/java/org/aquapackrobotics/sw8s/comms/SerialCommunicationUtility.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/SerialCommunicationUtility.java
@@ -4,48 +4,54 @@ package org.aquapackrobotics.sw8s.comms;
  * Provides helpers for creating and parsing messages
  * that the SW8 control board could use.
  */
-class SerialCommunicationUtility {
-    /**
-     * self.__ser.write(ControlBoard.START_BYTE)
-     *         for i in range(len(msg)):
-     *             c = msg[i:i+1]
-     *             if c == ControlBoard.START_BYTE or c == ControlBoard.END_BYTE or c == ControlBoard.ESCAPE_BYTE:
-     *                 self.__ser.write(ControlBoard.ESCAPE_BYTE)
-     *             self.__ser.write(c)
-     *         crc = Crc16.calcbytes(msg, byteorder='big')
-     *         self.__ser.write(crc)
-     *         self.__ser.write(ControlBoard.END_BYTE)
-     */
-
+public class SerialCommunicationUtility {
     private static final byte START_BYTE = (byte) 253;
     private static final byte END_BYTE = (byte) 254;
     private static final byte ESCAPE_BYTE = (byte) 255;
 
+    /**
+     * Takes in a raw message and converts it into the format a SW8 control board can use.
+     * @param message The raw message
+     * @return The message encoded in the format the SW8 control board uses
+     */
     public static byte[] constructMessage(byte[] message) {
         StringBuilder formattedMessage = new StringBuilder();
 
-        long crc = CRC.calculateCRC(CRC.Parameters.CRC16, message);
+        formattedMessage.append(START_BYTE);
 
+        // Add escaped message to formatted message
         for (byte msgByte : message) {
-            if (msgByte == START_BYTE || msgByte == END_BYTE || msgByte == ESCAPE_BYTE) {
-                formattedMessage.append(ESCAPE_BYTE);
-            }
-
-            formattedMessage.append(msgByte);
+            addEscapedByteToBuilder(formattedMessage, msgByte);
         }
 
-        // TODO THIS DOESN'T CONFORM TO SPEC, NEED TO ESCAPE & HIGH & LOW ORDER
-        formattedMessage.append(crc);
-        
+        // Add CRC to formatted message
+        long crc = CRC.calculateCRC(CRC.Parameters.CRC16, message);
+        short crc16 = (short) crc;
+        byte lowByte = (byte) (crc16 & 0x00FF);
+        byte highByte = (byte) ((crc16 & 0xFF00) >> 8);
+        addEscapedByteToBuilder(formattedMessage, highByte);
+        addEscapedByteToBuilder(formattedMessage, lowByte);
+
         formattedMessage.append(END_BYTE);
 
         return formattedMessage.toString().getBytes();
     }
-
+    /**
+     * Takes in an encoded message received from a SW8 control board and converts it into a usable format.
+     * @param message An encoded message from a control board
+     * @return The raw message extracted from the encoded message
+     */
     public static byte[] destructMessage(byte[] message) {
         StringBuilder deformattedMessage = new StringBuilder();
 
-
         return deformattedMessage.toString().getBytes();
+    }
+
+    private static void addEscapedByteToBuilder(StringBuilder builder, byte msgByte) {
+        if (msgByte == END_BYTE || msgByte == START_BYTE || msgByte == ESCAPE_BYTE) {
+            builder.append(msgByte);
+        }
+
+        builder.append(msgByte);
     }
 }
