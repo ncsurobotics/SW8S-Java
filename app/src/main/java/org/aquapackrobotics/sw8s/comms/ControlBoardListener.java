eb286b8994a43f4f483736c15f115703536df960
vpatil5 <vpatil5@ncsu.edu>
2022-10-03 21:19:47 -0400

    Added skeleton of ControlBoardListener

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/ControlBoardListener.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.aquapackrobotics.sw8s.comms;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.fazecast.jSerialComm.SerialPortMessageListener;
+
+/**
+ * The ControlBoardListener class implements SerialPortMessageListener to listen for messages starting with the START_BYTE.
+ * ControlBoardListener listens for messages from the Control Board
+ *
+ */
+public class ControlBoardListener implements SerialPortMessageListener {
+
+	public ControlBoardListener() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Returns the events for which serialEvent(SerialPortEvent) will be called
+	 */
+	@Override
+	public int getListeningEvents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	/**
+	 * Run when the specified ListeningEvent is triggered
+	 */
+	@Override
+	public void serialEvent(SerialPortEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Returns a boolean indicating whether the message delimiter indicates the end or the beginning of a message.
+	 * @return true if delimiter indicates end of message, false if delimiter indicates beginning
+	 */
+	@Override
+	public boolean delimiterIndicatesEndOfMessage() {
+		return false;
+	}
+	
+	/**
+	 * The delimiter that the listener will detect messages with
+	 */
+	@Override
+	public byte[] getMessageDelimiter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
