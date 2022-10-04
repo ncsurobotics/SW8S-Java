ee5fbb6e9ac6b90ea43faf590063ef6e4a7e11b7
Blokz101 <mquantwenty22@gmail.com>
2022-10-04 19:51:36 -0400

    Added documentation to each method

--- app/src/main/java/org/aquapackrobotics/sw8s/comms/ControlBoardThreadManager.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/ControlBoardThreadManager.java
@@ -6,10 +6,9 @@ import java.util.concurrent.*;
 
 public class ControlBoardThreadManager {
 
-    //TODO: JAVADOC
+    //Instance variables
     private ScheduledThreadPoolExecutor pool;
     private ControlBoardCommunication controlBoardCommunication;
-    private ControlBoardMode mode;
 
     //will represent serial comms later
     Runnable watchDog = new Runnable() {
@@ -19,25 +18,39 @@ public class ControlBoardThreadManager {
         }
     };
 
+    //Constructor
     public ControlBoardThreadManager(ScheduledThreadPoolExecutor pool) {
         this.pool = pool;
         startWatchDog();
         controlBoardCommunication = new ControlBoardCommunication();
     }
 
+    /**
+     * Schedules the watch dog thread runnable.
+     */
     private void startWatchDog() {
         pool.scheduleAtFixedRate(watchDog, 0, 500, TimeUnit.MILLISECONDS);
     }
 
-    private <V> ScheduledFuture<V> scheduleTask(Callable<V> b) throws ExecutionException, InterruptedException {
-        ScheduledFuture<V> sf = pool.schedule(b, 0, TimeUnit.MILLISECONDS);
-        return sf;
-    }
-
+    /**
+     * Utility function that waits for the result from a ScheduledFuture and returns it when it is available.
+     * @param <V> Variable type that the scheduledFuture will return.
+     * @param scheduledFuture ScheduledFuture that will be waited upon.
+     * @return The result from the scheduledFuture.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public <V> V waitForResult(ScheduledFuture<V> scheduledFuture) throws ExecutionException, InterruptedException {
         return scheduledFuture.get();
     }
 
+    /**
+     * Sets the mode of the control board.
+     * @param controlBoardMode The new disired controlBoardMode.
+     * @return ScheduledFuture that will return void.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public ScheduledFuture<Void> setMode(ControlBoardMode controlBoardMode) throws ExecutionException, InterruptedException {
         Callable<Void> modeRunnable = new Callable<Void>() {
             @Override
@@ -50,6 +63,12 @@ public class ControlBoardThreadManager {
         return scheduleTask(modeRunnable);
     }
 
+    /**
+     * Gets the current mode from the control board
+     * @return ScheduledFuture that will return the ControlBoardMode.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public ScheduledFuture<ControlBoardMode> getMode() throws ExecutionException, InterruptedException {
         Callable<ControlBoardMode> modeCallable = new Callable<ControlBoardMode>() {
             @Override
@@ -61,6 +80,20 @@ public class ControlBoardThreadManager {
         return scheduleTask(modeCallable);
     }
 
+    /**
+     * Set the inversions for each thruster individually.
+     * @param invert1 Boolean that the first thruster inversion will be set to.
+     * @param invert2 Boolean that the second thruster inversion will be set to.
+     * @param invert3 Boolean that the third thruster inversion will be set to.
+     * @param invert4 Boolean that the fourth thruster inversion will be set to.
+     * @param invert5 Boolean that the fifth thruster inversion will be set to.
+     * @param invert6 Boolean that the sixth thruster inversion will be set to.
+     * @param invert7 Boolean that the seventh thruster inversion will be set to.
+     * @param invert8 Boolean that the eighth thruster inversion will be set to.
+     * @return ScheduledFuture that will return void.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public ScheduledFuture<Void> setThrusterInversions(boolean invert1, boolean invert2, boolean invert3, boolean invert4, boolean invert5, boolean invert6, boolean invert7, boolean invert8) throws ExecutionException, InterruptedException {
         Callable<Void> inversionCallable = new Callable<Void>() {
             @Override
@@ -72,6 +105,12 @@ public class ControlBoardThreadManager {
        return scheduleTask(inversionCallable);
     }
 
+    /**
+     * Gets the current inversions from the control board.
+     * @return ScheduledFuture with a list of eight booleans which represents the inversion state for each motor respectively. 
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public ScheduledFuture<boolean[]> getThrusterInversions() throws ExecutionException, InterruptedException {
         Callable<boolean[]> inversionsGetter = new Callable<boolean[]>() {
             @Override
@@ -82,6 +121,21 @@ public class ControlBoardThreadManager {
 
         return scheduleTask(inversionsGetter);
     }
+
+    /**
+     * Sets the motor speeds individually and directly.
+     * @param speed1 Double for the first motor speed
+     * @param speed2 Double for the second motor speed
+     * @param speed3 Double for the third motor speed
+     * @param speed4 Double for the fourth motor speed
+     * @param speed5 Double for the fifth motor speed
+     * @param speed6 Double for the sixth motor speed
+     * @param speed7 Double for the seventh motor speed
+     * @param speed8 Double for the eighth motor speed
+     * @return ScheduledFuture that will return void.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public ScheduledFuture<Void> setMotorSpeeds(double speed1, double speed2, double speed3, double speed4, double speed5, double speed6, double speed7, double speed8) throws ExecutionException, InterruptedException {
         Callable<Void> speedsCallable = new Callable<Void>() {
             @Override
@@ -93,7 +147,21 @@ public class ControlBoardThreadManager {
        return scheduleTask(speedsCallable);
     }
 
+    //Closes controlBoardCommunication.
     public void dispose() {
         controlBoardCommunication.dispose();
+    }
+
+    /**
+     * Takes a callable as a parameter, schedules it to the pool with a 0 time delay, and returns its ScheduledFuture.
+     * @param <V>
+     * @param b
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private <V> ScheduledFuture<V> scheduleTask(Callable<V> b) throws ExecutionException, InterruptedException {
+        ScheduledFuture<V> sf = pool.schedule(b, 0, TimeUnit.MILLISECONDS);
+        return sf;
     }
 }
