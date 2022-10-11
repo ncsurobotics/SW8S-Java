381349a9c9fcd290d8e84ab354e8163aa088f8c8
ltvantil <lvantil@ncsu.edu>
2022-10-11 15:16:28 -0400

    Test deconstruct uses expected format. New CRC test.
    Assertarrayequals instead of assertequals to fix the issue of comparing addresses instead of values

--- app/src/test/java/org/aquapackrobotics/sw8s/comms/SerialCommunicationUtilityTest.java
+++ app/src/test/java/org/aquapackrobotics/sw8s/comms/SerialCommunicationUtilityTest.java
@@ -12,56 +12,77 @@ public class SerialCommunicationUtilityTest {
     private static final byte ESCAPE_BYTE = (byte) 255;
 
     // Message "MODEL"
-    private static byte[] encodedMessage_MODEL = new byte[] {START_BYTE,77,79,68,69,76,33,112,END_BYTE};
-    private static byte[] rawMessage_MODEL = new byte[] {77,79,68,69,76};
+    private static final byte[] encodedMessage_MODEL = new byte[] {77,79,68,69,76,33,112};
+    private static final byte[] rawMessage_MODEL = new byte[] {77,79,68,69,76};
 
     // Message "?TINV"
-    private static byte[] encodedMessage_TINV = new byte[] {START_BYTE,63,84,73,78,86,80,29,END_BYTE};
-    private static byte[] rawMessage_TINV = new byte[] {START_BYTE,63,84,73,78,86,END_BYTE};
+    private static final byte[] encodedMessage_TINV = new byte[] {63,84,73,78,86,80,29};
+    private static final byte[] rawMessage_TINV = new byte[] {63,84,73,78,86};
 
-    // Message
-    public static byte[] encodedMessage_Zeros = new byte[] {START_BYTE,82,65,87,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,96,57,END_BYTE};
-    public static byte[] rawMessage_Zeros = new byte[] {82,65,87,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    // Message zeros
+    public static final byte[] encodedMessage_Zeros = new byte[] {82,65,87,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,96,57};
+    public static final byte[] rawMessage_Zeros = new byte[] {82,65,87,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
     @Test
     public void testDestructModel() {
-        Assert.assertEquals(rawMessage_MODEL,
+        Assert.assertArrayEquals(rawMessage_MODEL,
                 SerialCommunicationUtility.destructMessage(encodedMessage_MODEL));
     }
 
     @Test
     public void testDestructTINV() {
-        Assert.assertEquals(rawMessage_TINV,
+        Assert.assertArrayEquals(rawMessage_TINV,
                 SerialCommunicationUtility.destructMessage(encodedMessage_TINV));
     }
 
     @Test
     public void testDestructZeros() {
-        Assert.assertEquals(rawMessage_Zeros,
+        Assert.assertArrayEquals(rawMessage_Zeros,
                 SerialCommunicationUtility.destructMessage(encodedMessage_Zeros));
     }
 
     @Test
     public void testConstructModel() {
-        Assert.assertEquals(encodedMessage_MODEL,
+        Assert.assertArrayEquals(appendStartEndMarkers(encodedMessage_MODEL),
                 SerialCommunicationUtility.constructMessage(rawMessage_MODEL));
     }
 
     @Test
     public void testConstructTINV() {
-        Assert.assertEquals(encodedMessage_TINV,
+        Assert.assertArrayEquals(appendStartEndMarkers(encodedMessage_TINV),
                 SerialCommunicationUtility.constructMessage(rawMessage_TINV));
     }
 
     @Test
     public void testConstructZeros() {
-        Assert.assertEquals(encodedMessage_Zeros,
+        Assert.assertArrayEquals(appendStartEndMarkers(encodedMessage_Zeros),
                 SerialCommunicationUtility.constructMessage(rawMessage_Zeros));
+    }
+
+    @Test
+    public void testCRC16() {
+        short crc16 = CRC.CITT16_False(rawMessage_MODEL, rawMessage_MODEL.length);
+        byte lowByte = (byte) (crc16 & 0x00FF);
+        byte highByte = (byte) ((crc16 & 0xFF00) >> 8);
+
+        Assert.assertEquals(highByte, encodedMessage_MODEL[encodedMessage_MODEL.length - 2]);
+        Assert.assertEquals(lowByte, encodedMessage_MODEL[encodedMessage_MODEL.length - 1]);
     }
 
     @Test
     public void testMessageMarkers() {
         Assert.assertTrue(SerialCommunicationUtility.isStartOfMessage(START_BYTE));
         Assert.assertTrue(SerialCommunicationUtility.isEndOfMessage(END_BYTE));
+    }
+
+    private byte[] appendStartEndMarkers(byte[] original) {
+        byte[] wrappedOriginal = new byte[original.length + 2];
+
+        System.arraycopy(original, 0, wrappedOriginal, 1, original.length);
+
+        wrappedOriginal[0] = START_BYTE;
+        wrappedOriginal[wrappedOriginal.length - 1] = END_BYTE;
+
+        return wrappedOriginal;
     }
 }
