750e1234ffcc829d446ae61de5a2a4b08cbd3054
Derek Chang <dchang1712@gmail.com>
2022-10-12 20:57:23 -0400

    Raw_Test added motorNumber and speed input

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/missions/Local_Test.java
@@ -0,0 +1,47 @@
+package org.aquapackrobotics.sw8s.missions;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+import org.aquapackrobotics.sw8s.states.*;
+import org.aquapackrobotics.sw8s.trainingsim.SimWindow;
+
+import java.util.Scanner;
+
+public class Local_Test extends Mission {
+    Scanner scnr = new Scanner(System.in);
+
+    public Local_Test(ScheduledThreadPoolExecutor pool) {
+        super(pool);
+    }
+
+    // TODO: implement
+    @Override
+    protected State initialState() {
+        return null;
+    }
+
+    // TODO: implement
+    @Override
+    protected void executeState(State state) {
+        boolean continue = true;
+        while (continue) {
+            System.out.print("Direction, Power, Seconds (i.e. Right 0.5 3): ");
+            String nextLine = scnr.nextLine();
+            if (nextLine.equals("")) {
+                return;
+            }
+
+            String[] lineParts = nextLine.split(" ");
+            
+            String direction = lineParts[0];
+            double power = Double.valueOf(lineParts[1]);
+            double seconds = Double.valueOf(lineParts[2]);
+        }
+    }
+
+    // TODO: implement
+    @Override
+    protected State nextState(State state) {
+        return null;
+    }
+}
