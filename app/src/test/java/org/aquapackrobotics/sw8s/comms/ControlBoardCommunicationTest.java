02e9f22c5b43209327ff8ae1a11164b67c25067b
Achyuta Kannan <ackannan@ncsu.edu>
2022-10-14 14:35:37 -0400

    added communications test file

---
+++ app/src/test/java/org/aquapackrobotics/sw8s/comms/ControlBoardCommunicationTest.java
@@ -0,0 +1,11 @@
+package org.aquapackrobotics.sw8s.comms;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class ControlBoardCommunicationTest {
+    @BeforeEach
+    public void setup() {
+
+    }
+
+}
