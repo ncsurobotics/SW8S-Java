27cb9836bfc67384031a7d063041abf24d87f284
Derek Chang <dchang1712@gmail.com>
2022-10-14 13:02:48 -0400

    Changed print spacing

--- app/src/main/java/org/aquapackrobotics/sw8s/missions/Local_Test.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/missions/Local_Test.java
@@ -16,15 +16,17 @@ public class Local_Test extends Mission {
     // TODO: implement
     @Override
     protected State initialState() {
+        System.out.println("initState reached");
         return new InitState(pool);
     }
 
     // TODO: implement
     @Override
     protected void executeState(State state) {
+        System.out.println("executeState reached");
         boolean cont = true;
         while (cont) {
-            System.out.print("Direction, Power, Seconds (i.e. Right 0.5 3): ");
+            System.out.println("Direction, Power, Seconds (i.e. Right 0.5 3): ");
             String nextLine = scnr.nextLine();
             if (nextLine.equals("")) {
                 return;
@@ -35,6 +37,15 @@ public class Local_Test extends Mission {
             String direction = lineParts[0];
             double power = Double.valueOf(lineParts[1]);
             double seconds = Double.valueOf(lineParts[2]);
+
+            /* 
+            switch (direction.toLowerCase()) {
+                case "left": 
+                    //setMotorSpeed
+                case "right":
+                    //setMotorSpeed to cause right velocity
+
+            }*/
         }
     }
 
