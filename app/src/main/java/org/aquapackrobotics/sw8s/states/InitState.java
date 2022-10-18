fbe15fdd60de0d8a227612c16b042fc0d9ce49a2
ltvantil <lvantil@ncsu.edu>
2022-10-17 20:04:55 -0400

    Revert "Removed bin"
    This reverts commit be06b781ce4f55de148b05f1ac2d08bad24600e2.

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/InitState.java
@@ -0,0 +1,27 @@
+package org.aquapackrobotics.sw8s.states;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class InitState extends State {
+    public InitState(ScheduledThreadPoolExecutor pool) {
+        super(pool);
+    }
+
+    // TODO: implement
+    public void onEnter() {
+    }
+
+    // TODO: implement
+    public boolean onPeriodic() {
+        return false;
+    }
+
+    // TODO: implement
+    public void onExit() {
+    }
+
+    // TODO: implement
+    public State nextState() {
+        return null;
+    }
+}
