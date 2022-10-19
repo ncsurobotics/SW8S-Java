38fc0c90ebda0018728c46cca0ca68047888c987
arfnoor <adam.rf.noor@gmail.com>
2022-10-18 20:53:33 -0400

    add stop button and made slightly pretty
    Co-authored-by: Aaron Green

--- app/src/main/java/org/aquapackrobotics/sw8s/App.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/App.java
@@ -5,7 +5,8 @@ package org.aquapackrobotics.sw8s;
 import javax.swing.*;
 import java.awt.*;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import org.aquapackrobotics.sw8s.missions.Mission;
 import org.aquapackrobotics.sw8s.missions.AutoMission;
 import org.aquapackrobotics.sw8s.missions.ManualMission;
@@ -20,6 +21,7 @@ public class App {
     public static void main(String[] args) {
         ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(POOLSIZE);
         String currentMission = "N/A";
+        String currentState = "N/A";
         String helpFlag[] = {
             "\nBasic Utility:", 
             "\n'--help' or '-h' -- displays list of command flags",
@@ -34,10 +36,11 @@ public class App {
         for (String str: args) {
             switch (str) {
                 case "--test":
+                    currentMission = "None, running test";
                     System.out.println("Yay! it worked!");
                     break;
                 case "--testmission":
-                    currentMission = "test";
+                    currentMission = "Test Mission";
                     break;
                 case "--currentMission":
                     System.out.println(currentMission);
@@ -49,10 +52,12 @@ public class App {
                     }
                     break;
                 // case "--raw_test":
+                //     currentMission = "Raw Test";
                 //     Mission missionRaw_Test = (Mission) new Raw_TestMission(pool);
                 //     missionRaw_Test.run();
                 //     break;
                 // case "--local_test":
+                //     currentMission = "Local Test";
                 //     Mission missionLocal_Test = (Mission) new Local_TestMission(pool);
                 //     missionLocal_Test.run();
                 //     break;
@@ -66,39 +71,74 @@ public class App {
                 //     executeState(State3);
                 //     break;
                 case "manual":
+                    currentMission = "Manual";
                     Mission missionManual = (Mission) new ManualMission(pool);
                     missionManual.run();
                     break;
                 default:
+                    currentMission = "Auto";
                     Mission missionAuto = (Mission) new AutoMission(pool);
                     missionAuto.run();
                     break;
 
             }
         }
+
+        
+        // Variables
+        Color red = new Color(255, 0, 0);
+        Color darkGrey = new Color(80, 80, 80);
+        Color white = new Color(255, 255, 255);
+       
         //Creating the Frame
         JFrame frame = new JFrame("Chat Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-         //Creating the panel at bottom and adding components
-         JPanel panel = new JPanel(); // the panel is not visible in output
-         JLabel label = new JLabel("Enter Text");
-         JTextField tf = new JTextField(10); // accepts upto 10 characters
-         JButton send = new JButton("Send");
-         panel.add(label); // Components Added using Flow Layout
-         panel.add(tf);
-         panel.add(send);
- 
-         // Text Area at the Center
-         JTextArea ta = new JTextArea();
-         ta.setText("Mission: " + "N/A" + 
-                    "\nState: " + "N/A" +
-                    "");
- 
-         //Adding Components to the frame.
-         frame.getContentPane().add(BorderLayout.SOUTH, panel);
-         frame.getContentPane().add(BorderLayout.CENTER, ta);
-         frame.setVisible(true);
+        frame.setSize(800, 800);
+       
+        //Low Panel and Content
+        JPanel lowPanel = new JPanel(); // the panel is not visible in output
+        lowPanel.setBackground(darkGrey);
+        JButton eStop = new JButton("STOP");
+        eStop.setPreferredSize(new Dimension(300, 100));
+        eStop.setBackground(red);
+        eStop.setForeground(white);
+        eStop.setFont(new Font(Font.DIALOG, Font.BOLD, 50));
+        eStop.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TO IMPLEMENT: ALL ZEROES, MAKE ROBOT NOT GO **BEFORE** SYSTEM.EXIT
+                System.exit(0);
+            }
+        });
+        lowPanel.add(eStop);
+
+        //High Panel and Content
+        JPanel highPanel = new JPanel();
+        highPanel.setBackground(darkGrey);
+        JLabel label = new JLabel("Enter Text");
+        label.setFont(new Font(Font.DIALOG, Font.BOLD, 20));
+        label.setForeground(white);
+        JTextField tf = new JTextField(50); // accepts upto 100 characters
+        JButton sendButton = new JButton("Send");
+        highPanel.add(label);
+        highPanel.add(tf);
+        highPanel.add(sendButton);
+        
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+        ta.setText("Mission: " + currentMission + 
+                 "\nState:   " + currentState +
+                   "");
+        Font  f  = new Font(Font.DIALOG,  Font.BOLD, 50);
+        ta.setFont(f);
+        
+        //Adding Panel/Text Area to the frame.
+        frame.getContentPane().add(BorderLayout.NORTH, highPanel);
+        frame.getContentPane().add(BorderLayout.SOUTH, lowPanel);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
         
     }
 }
