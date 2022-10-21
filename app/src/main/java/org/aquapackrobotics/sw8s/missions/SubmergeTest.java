fddc0c9f975d99e28c96005df20c1d8062be1896
Dennis Chen <dchen27@ncsu.edu>
2022-10-21 14:00:33 -0400

    Added SubmergeTest and SubmergeTestStates

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/missions/SubmergeTest.java
@@ -0,0 +1,38 @@
+//to build gradle do: .\gradlew.bat build
+//to run grade do: .\gradlew.bat run
+package org.aquapackrobotics.sw8s.missions;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import org.aquapackrobotics.sw8s.missions.*;
+import org.aquapackrobotics.sw8s.comms.*;
+import org.aquapackrobotics.sw8s.states.*;
+import org.aquapackrobotics.sw8s.states.SubmergeTestStates.*;
+
+import java.util.concurrent.*;
+/**
+ * State machine for testing motors
+ */
+public class SubmergeTest extends Mission {
+
+    public SubmergeTest(ScheduledThreadPoolExecutor pool) {
+        super(pool);
+
+    }
+
+    @Override
+    protected State initialState(){
+        return new SubmergeTestInitState(pool);
+    }
+
+    @Override
+    protected void executeState(State state) throws ExecutionException, InterruptedException  {
+        while (state.onPeriodic()) {
+            
+        }
+    }
+
+    @Override
+    protected State nextState(State state) {
+        return state.nextState();
+    }
+}
