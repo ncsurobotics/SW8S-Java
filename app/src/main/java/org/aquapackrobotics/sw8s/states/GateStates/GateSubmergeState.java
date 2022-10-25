2deb5ce6175c0abd29ff940a6061e7ea541ee421
Dennis Chen <dchen27@ncsu.edu>
2022-10-24 20:17:44 -0400

    Added Gate state machine and dependant states

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/GateStates/GateSubmergeState.java
@@ -0,0 +1,42 @@
+package org.aquapackrobotics.sw8s.states.GateStates;
+
+import org.aquapackrobotics.sw8s.comms.*;
+import org.aquapackrobotics.sw8s.states.*;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.*;
+
+public class GateSubmergeState extends State {
+
+    ControlBoardThreadManager manager;
+    long startTime;
+    long endTime;
+
+    public GateSubmergeState(ScheduledThreadPoolExecutor pool) {
+        super(pool);
+        manager = new ControlBoardThreadManager(pool);
+    }
+
+    public void onEnter() throws ExecutionException, InterruptedException {
+        manager.setMode(ControlBoardMode.LOCAL);
+        manager.setThrusterInversions(true, true, false, false, true, false, false, true);
+        manager.setLocalSpeeds(0, 0, -0.5, 0, 0, 0);
+        startTime = System.currentTimeMillis();
+    }
+
+
+    public boolean onPeriodic() {
+        endTime = System.currentTimeMillis();
+        if (endTime - startTime >= 1500 ) {
+            return false;
+        }
+        return true;
+    }
+
+    public void onExit() throws ExecutionException, InterruptedException{
+        manager.setLocalSpeeds(0, 0, 0, 0, 0, 0);
+    }
+
+    public State nextState() {
+        return new GateForwardState(pool);
+    }
+}
