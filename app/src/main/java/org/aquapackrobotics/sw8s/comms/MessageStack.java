50e1cbbd54bd02958820d947f9cdd6179c5ec0a2
vpatil5 <vpatil5@ncsu.edu>
2022-10-31 21:01:05 -0400

    Updated MessageStack to use LinkedBlockingDeque

--- app/src/main/java/org/aquapackrobotics/sw8s/comms/MessageStack.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/MessageStack.java
@@ -1,17 +1,14 @@
 package org.aquapackrobotics.sw8s.comms;
 
-import java.util.Stack;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.BinaryOperator;
-import java.util.function.UnaryOperator;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class MessageStack {
     private static MessageStack ms;
-    private AtomicReference<Stack<String>> messages;
+    private LinkedBlockingDeque<String> messages;
 
 
     private MessageStack() {
-        messages = new AtomicReference<Stack<String>>();
+        messages = new LinkedBlockingDeque<String>();
     }
 
     public static MessageStack getInstance() {
@@ -22,22 +19,13 @@ public class MessageStack {
     }
 
     public void push(String message) {
-    	Stack<String> stack = new Stack<String>();
-    	stack.push(message);
-    	
-    	//This operator pushes the top element of the second stack onto the first and returns it
-    	BinaryOperator<Stack<String>> combine = (stack1, stack2) -> {
-    		stack1.push(stack2.pop());
-    		return stack1;
-    	};
-    	
-    	//This pushes the message onto the message stack
-    	messages.accumulateAndGet(stack, combine);
+    	//This adds the message onto the message deque
+    	messages.add(message);
     }
     
     public String pop() {
-    	//Should probably rewrite to use an atomic function to update the message stack
-    	return messages.get().pop();
+    	//Returns the first message stored in the deque
+    	return messages.remove();
     }
     
 }
