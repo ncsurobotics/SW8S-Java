5b77c35db957d5e2c1c35b1cace47eb7a179b134
vpatil5 <vpatil5@ncsu.edu>
2022-10-31 22:42:51 -0400

    Added timeout feature to MessageStack.pop()

--- app/src/main/java/org/aquapackrobotics/sw8s/comms/MessageStack.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/MessageStack.java
@@ -1,6 +1,7 @@
 package org.aquapackrobotics.sw8s.comms;
 
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class MessageStack {
     private static MessageStack ms;
@@ -17,15 +18,26 @@ public class MessageStack {
         }
         return ms;
     }
-
+    
+    /**
+     * Puts the given message at the front of the message deque
+     * @param message message to add
+     */
     public void push(String message) {
     	//This adds the message onto the message deque
-    	messages.add(message);
+    	messages.addFirst(message);
     }
     
-    public String pop() {
+    /**
+     * Removes the first element of the deque and returns it. Also features a timeout if no element is available.
+     * @param timeout the length of the timeout
+     * @param unit the time unit the timeout is specified in
+     * @return the first element of the deque, or null if no element during specified timeout
+     * @throws InterruptedException if interrupted while waiting for element to become available
+     */
+    public String pop(long timeout, TimeUnit unit) throws InterruptedException {
     	//Returns the first message stored in the deque
-    	return messages.remove();
+    	return messages.pollFirst(timeout, unit);
     }
     
 }
