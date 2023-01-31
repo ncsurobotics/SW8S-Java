259b751c2a34c8b3cffc99c79b5c72fa18290426
vpatil5 <vpatil5@ncsu.edu>
2023-01-30 20:48:47 -0500

    Changed DataBuffer to use floats

--- app/src/main/java/org/aquapackrobotics/sw8s/comms/DataBuffer.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/comms/DataBuffer.java
@@ -4,16 +4,15 @@
 package org.aquapackrobotics.sw8s.comms;
 
 /**
- * Data Buffer is used to store data that is received quickly in order to filter out outliers.
+ * Data Buffer is used to store data (of type float) that is received quickly in order to filter out outliers.
  * @author vaibh
  *
- * @param E the type of Data stored in the buffer;
  */
-public class DataBuffer<E> {
+public class DataBuffer {
 	
 	private static int BUFFER_CAPACITY = 10;
 	
-	private E[] buffer;
+	private float[] buffer;
 	
 	private int front;
 	
@@ -23,31 +22,45 @@ public class DataBuffer<E> {
 	
 	@SuppressWarnings("unchecked")
 	public DataBuffer() {
-		buffer = (E[]) new Object[BUFFER_CAPACITY];
+		buffer = new float[BUFFER_CAPACITY];
 		size = 0;
 		front = 0;
 		rear = 0;
 	}
 	
-	public void enqueue(E data) {
+	/**
+	 * Adds data to the buffer, removing the last element if capacity has been reached.
+	 * @param data the element to add
+	 */
+	public void enqueue(float data) {
 		if (size == BUFFER_CAPACITY) {
 			removeLast();
 		}
 		buffer[rear] = data;
 		rear = (rear + 1) % buffer.length;
-
+		size++;
 	}
 	
+	/**
+	 * Removes the last element in the buffer
+	 */
 	private void removeLast() {
 		front = (front + 1) % buffer.length;
 		size--;
 	}
 	
-	private void getAverage() {
-		float sum;
+	/**
+	 * Returns the average of the data contained in the buffer, removing any outliers.
+	 * @return the average of the data contained in the buffer, removing any outliers.
+	 */
+	private float getAverage() {
+		//TODO: Remove outliers
+		
+		float sum = 0;
 		for (int i = front; i != rear; i = (i + 1) % buffer.length) {
-			sum = buffer[i].getData();
+			sum = buffer[i];
 		}
+		return sum / size;
 	}
 	
 	
