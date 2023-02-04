ed5a74de43ece8fa590fead17e2e6277275ee911
Bennett Petzold <bennettpetzold01@gmail.com>
2023-02-04 12:19:28 -0500

    Add read test states

---
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/ReceiveTestStates/Depth.java
@@ -0,0 +1,50 @@
+package org.aquapackrobotics.sw8s.states;
+
+
+import org.aquapackrobotics.sw8s.states.*;
+import org.aquapackrobotics.sw8s.comms.*;
+
+import java.util.Arrays;
+
+import java.util.concurrent.*;
+
+public class Depth extends State {
+    ScheduledFuture<byte[]> depthRead;
+
+    public Depth(ControlBoardThreadManager manager) {
+        super(manager);
+    }
+
+    // TODO: implement
+    public void onEnter() {
+        try {
+            depthRead = manager.MS5837Read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TODO: implement
+    public boolean onPeriodic() {
+        if ( depthRead.isDone() ) {
+            try {
+                System.out.println("Depth: " +
+                    Arrays.toString(depthRead.get()));
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // TODO: implement
+    public void onExit() {
+    }
+
+    // TODO: implement
+    public State nextState() {
+        return new Gyro(manager);
+    }
+}
