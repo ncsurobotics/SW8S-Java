12a326409642c8ce2ff24ade32fa0120272d899f
Bennett Petzold <bennettpetzold01@gmail.com>
2023-02-04 12:23:51 -0500

    Set ReceiveTest to use the test states

--- app/src/main/java/org/aquapackrobotics/sw8s/missions/ReceiveTest.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/missions/ReceiveTest.java
@@ -17,67 +17,18 @@ public class ReceiveTest extends Mission {
     // TODO: implement
     @Override
     protected State initialState() {
-        return new InitState(manager);
+        return new Depth(manager);
     }
 
     // TODO: implement
     @Override
     protected void executeState(State state) throws ExecutionException, InterruptedException  {
-        Scanner scnr = new Scanner(System.in);
-
-        manager.setThrusterInversions(true, true, false, false, true, false, false, true);
-
-        boolean cont = true;
-        while (cont) {
-            System.out.println("Direction, Power (i.e. Right 0.5 3): ");
-            String nextLine;
-            if (scnr.hasNextLine()) {
-                nextLine = scnr.nextLine();
-                if (nextLine.equals("")) {
-                    manager.setLocalSpeeds(0,0,0,0,0,0);
-                    System.out.println("All motors set to 0.");
-                    return;
-                }
-                String[] lineParts = nextLine.split(" ");
-                
-                String direction = lineParts[0];
-                double power = Double.valueOf(lineParts[1]);
-
-                switch (direction.toLowerCase()) {
-                    case "left": 
-                        manager.setLocalSpeeds(-power, 0, 0, 0, 0, 0);
-                        break;
-                    case "right":
-                        manager.setLocalSpeeds(power, 0, 0, 0, 0, 0);
-                        break;
-                    case "forward":
-                        manager.setLocalSpeeds(0, power, 0, 0, 0, 0);
-                        break;
-                    case "backward":
-                        manager.setLocalSpeeds(0, -power, 0, 0, 0, 0);
-                        break;
-                    case "up":
-                        manager.setLocalSpeeds(0, 0, power, 0, 0, 0);
-                        break;
-                    case "down":
-                        manager.setLocalSpeeds(0, 0, -power, 0, 0, 0);
-                        break;
-                    case "pitch":
-                        manager.setLocalSpeeds(0, 0, 0, power, 0, 0);
-                        break;
-                    case "roll":
-                        manager.setLocalSpeeds(0, 0, 0, 0, power, 0);
-                        break;
-                    case "yaw":
-                        manager.setLocalSpeeds(0, 0, 0, 0, 0, power);
-                        break;
-                }
-            }
-        }
+        while ( !state.onPeriodic() );
     }
+
     // TODO: implement
     @Override
     protected State nextState(State state) {
-        return null;
+        return state.nextState();
     }
 }
--- app/src/main/java/org/aquapackrobotics/sw8s/states/ReceiveTestStates/Depth.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/ReceiveTestStates/Depth.java
@@ -33,7 +33,7 @@ public class Depth extends State {
                 return true;
             } catch (Exception e) {
                 e.printStackTrace();
-                return false;
+                return true;
             }
         }
         return false;
