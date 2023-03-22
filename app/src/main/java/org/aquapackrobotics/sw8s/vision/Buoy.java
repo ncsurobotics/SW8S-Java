e673efe17be240cafd7b4b3c68dc549e82a24608
Xingjian Li <lixingjian2002@gmail.com>
2023-03-21 20:12:01 -0400

    comments on how to use this file

--- app/src/main/java/org/aquapackrobotics/sw8s/vision/Buoy.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/vision/Buoy.java
@@ -2,6 +2,27 @@ package org.aquapackrobotics.sw8s.vision;
 
 /**
  * Code for "Mark the Grade" task
+ * 
+ * Usage:
+ * 		// init (load the model)
+ * 		private Buoy buoy_process = new Buoy();
+ * 		
+ * 		// optional initialization
+ * 		// select which target is wanted in transAlign(), second takes priority if both true
+ * 		buoy_process.<first/second> = true/false; // default both true
+ * 		
+ * 
+ * 		// detect where the buoy is, it returns the bounding box image if you want
+ * 		Mat detected_image = buoy_process.detectYoloV5(image);
+ * 		
+ * 		// process numerical values on where the target is
+ * 		// align for translation vector (image_x, image_y, distance approximation)
+ * 		buoy_process.transAlign(); 
+ * 
+ * 		// access numerical values
+ * 			buoy_process.translation
+ * 			buoy_process.rotation // currently not considered
+ * 		
  * @author Xingjian Li
  *
  */
@@ -12,10 +33,12 @@ public class Buoy extends nn_cv2 {
 	private final int second_classid = 1;
 	public boolean first = true;
 	public boolean second = true;
+	
 	// + left, - right [-1,1]
 	// + up, - down [-1,1]
-	// distance [0,1], higher means further away
+	// distance [0,1], higher means further away, based on object height
 	public double[] translation = {0, 0, 0};
+	
 	public double[] rotation = {0, 0, 0};
 
 	// load buoy specific network
