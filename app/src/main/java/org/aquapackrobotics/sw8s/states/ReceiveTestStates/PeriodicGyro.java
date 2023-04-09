04656da45a8d24aebc790668a93c8b532a61729f
Bennett Petzold <bennettpetzold01@gmail.com>
2023-04-09 18:34:24 -0400

    Update gyro printout type for arrays

--- app/src/main/java/org/aquapackrobotics/sw8s/states/ReceiveTestStates/PeriodicGyro.java
+++ app/src/main/java/org/aquapackrobotics/sw8s/states/ReceiveTestStates/PeriodicGyro.java
@@ -30,7 +30,7 @@ public class PeriodicGyro extends State {
     public boolean onPeriodic() {
         if ( gyroRead.isDone() ) {
             System.out.println("PeriodicGyro X: " +
-                Float.toString(manager.getGyrox()));
+                Arrays.toString(manager.getGyrox()));
             if (++loop_runs < 10) {
                 return false;
             }
