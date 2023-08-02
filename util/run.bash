4e8694896865c1da41aa47eb46f1630aa87d98b5
Bennett Petzold <bennettpetzold01@gmail.com>
2023-08-02 17:23:24 -0400

    Remove nohup from run

--- util/run.bash
+++ util/run.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tar --overwrite -xf "$(dirname $0)"/../app/build/distributions/app-unspecified.tar -C /tmp &&
-	nohup /tmp/app-unspecified/bin/app "${@:1}"
+	/tmp/app-unspecified/bin/app "${@:1}"
